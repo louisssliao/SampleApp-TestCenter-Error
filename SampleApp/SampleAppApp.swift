d3cf2e93aa4325be00d9e3bff7f8457d49f2555d
louis.liao <louis.liao@grindr.com>
2022-09-19 17:41:16 +0800

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Louis Liao on 2022/9/19.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
