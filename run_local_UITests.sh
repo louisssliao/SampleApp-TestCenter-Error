835e9be7b6776f852ad72d256c31c70a8fceb384
louis.liao <louis.liao@grindr.com>
2022-09-20 10:35:03 +0800

    clean code

--- run_local_UITests.sh
+++ run_local_UITests.sh
@@ -1,11 +1,11 @@
 # This script uses the same setup as Jenkins and allows Devs to run UI test via Fastlane locally.
 
 export LANG="en_US.UTF-8"
-export DEVELOPER_DIR=$(xcode-select -p)
+export DEVELOPER_DIR="/Applications/Xcode-14.app/Contents/Developer"
 export XCUITESTS_WIREMOCK_HOST_URL="http://localhost:8080"
 export XCUITESTS_INSTANCE_DIR="${HOME}/Derived_Data/SampleApp_Test"
-export XCUITESTS_DEVICE="iPhone 12 (16.0)"
-export XCUITESTS_DEVICE_NAME="iPhone 12"
+export XCUITESTS_DEVICE="iPhone 13 Pro (16.0)"
+export XCUITESTS_DEVICE_NAME="iPhone 13 Pro"
 export XCUITESTS_DEVICE_RUNTIME="com.apple.CoreSimulator.SimRuntime.iOS-16-0"
 export XCUITESTS_TRY_COUNT="1"
 export XCUITESTS_PARALLEL_TESTRUN_COUNT="2"
